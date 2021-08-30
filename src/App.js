063aa156a6ca995388d8762c9aad305840545e32
guruprasad <guruprasadsali378@gmail.com>
2021-08-30 21:10:30 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import CreateForm from './PracticeComponent/CreateForm';
 // import Hemant_TodoFinal from './PracticeComponent/Hemant_TodoFinal'
 // import ShubhamDeqode from './PracticeComponent/ShubhamDeqode'
-import Todo30aug from './PracticeComponent/Todo30aug'
+// import Todo30aug from './PracticeComponent/Todo30aug'
+import SelfTodo from './PracticeComponent/SelfTodo'
 function App() {
   return (
     <div className="App">
     {/* <CreateForm/> */}
     {/* <Hemant_TodoFinal/> */}
     {/* <ShubhamDeqode/> */}
-    <Todo30aug/>
+    {/* <Todo30aug/> */}
+    <SelfTodo/>
     </div>
   );
 }
